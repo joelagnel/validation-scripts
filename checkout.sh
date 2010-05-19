75156b3d5edf2cc868df98b2cdc866d6fd6ab0a1
Jason Kridner <jkridner@beagleboard.org>
2010-05-19 14:56:29 -0500

    Added comments for the trees being tracked

--- checkout.sh
+++ checkout.sh
@@ -16,8 +16,11 @@ mkdir -p $DIR/x-load
 cd $DIR/x-load
 git init
 git remote add origin $ORIGIN/x-load.git
+# Steve Sakoman performs public maintainership of x-load for BeagleBoard and Gumstix Overo
 git remote add sakoman git://gitorious.org/x-load-omap3/mainline.git
+# X-load for the OMAP Zoom/Blaze mobile development platforms from TI
 git remote add omapzoom $OMAPZOOM/x-loader.git
+# X-load for the OMAP35x EVM, AM3517 EVM, and AM/DM37x plaforms from TI
 git remote add psp $PSP2/x-load-omap3.git
 git remote update
 git checkout -b omapzoom/master --track omapzoom/master
@@ -32,10 +35,15 @@ mkdir -p $DIR/u-boot
 cd $DIR/u-boot
 git init
 git remote add origin $ORIGIN/u-boot.git
+# This is the u-boot mainline where everything should be headed
 git remote add upstream $DENX/u-boot.git
+# This is where TI stages changes for upstream
 git remote add upstream-ti $DENX/u-boot-ti.git
+# Steve Sakoman performs public maintainership of u-boot for BeagleBoard and Gumstix Overo
 git remote add sakoman git://gitorious.org/u-boot-omap3/mainline.git
+# U-boot for the OMAP Zoom/Blaze mobile development platforms from TI
 git remote add omapzoom $OMAPZOOM/u-boot.git
+# U-boot for the OMAP35x EVM, AM3517 EVM, and AM/DM37x plaforms from TI
 git remote add psp $PSP2/u-boot-omap3.git
 git remote update
 git checkout -b upstream/master --track upstream/master
@@ -53,12 +61,19 @@ mkdir -p $DIR/linux-2.6
 cd $DIR/linux-2.6
 git init
 git remote add origin $ORIGIN/linux.git
+# This is Linux
 git remote add upstream $KERNEL/torvalds/linux-2.6.git
+# This is where patches are staged for Linus
 git remote add linux-omap $KERNEL/tmlind/linux-omap-2.6.git
+# This is a working tree for power management code
 git remote add linux-omap-pm $KERNEL/khilman/linux-omap-pm.git
+# Linux for the OMAP35x EVM, AM3517 EVM, and AM/DM37x plaforms from TI
 git remote add psp $PSP/sriram/ti-psp-omap.git
+# Work on-going to improve the V4L support
 git remote add psp-video $PSP/vaibhav/ti-psp-omap-video.git
+# Koen's kernel for the Angstrom distribution
 git remote add koen git://gitorious.org/angstrom/angstrom-linux.git
+# A kernel patched to support Android as part of the ARowboat.org project
 git remote add rowboat $ROWBOAT/kernel.git
 git remote update
 git checkout -b upstream/master --track upstream/master
