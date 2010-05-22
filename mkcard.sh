ff4809ea3fa83676d6fa1a4ca1209d3fc496cde8
Jason Kridner <jkridner@beagleboard.org>
2010-05-23 01:22:46 +0200

    Patch needs cleaning

--- mkcard.sh
+++ mkcard.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # mkcard.sh v0.3.beagleboard-validation-scripts.1
 # (c) Copyright 2009 Graeme Gregory <dp@xora.org.uk>
 # Licensed under terms of GPLv2
@@ -14,60 +14,71 @@ LOCALSRC=/media/mmcblk0
 WGETSRC=http://www.beagleboard.org/~arago/xm-testing
 DST=/media/target
 
-function do_clean {
-dd if=/dev/zero of=$DRIVE bs=1024 count=1024
-}
-
-function do_format {
-for tool in sfdisk mkfs.vfat mke2fs; do
+for tool in dd sfdisk partx mkfs.vfat mke2fs; do
 	if ! type $tool >/dev/null 2>&1; then
 		echo "ERROR: \"$tool\" not found."
 		echo "	Try 'opkg install dosfstools e2fsprogs e2fsprogs-mke2fs'"
 		exit 2
 	fi
 done
+
+function do_clean {
+sleep 3
+umount ${DRIVE}1
+umount ${DRIVE}2
+umount ${DRIVE}p1
+umount ${DRIVE}p2
+dd if=/dev/zero of=$DRIVE bs=1024 count=1024
+}
+
+function do_format {
 SIZE=`fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
 echo DISK SIZE - $SIZE bytes
 CYLINDERS=`echo $SIZE/255/63/512 | bc`
 echo CYLINDERS - $CYLINDERS
+sync
+sleep 3
 {
 echo ,9,0x0C,*
 echo ,,,-
 } | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
-sync
-sleep 1
+partx /dev/sda
 if [ -b ${DRIVE}1 ]; then
-	umount ${DRIVE}1
-	mkfs.vfat -F 32 -n "boot" ${DRIVE}1
+	DRIVE1=${DRIVE}1
 else
 	if [ -b ${DRIVE}p1 ]; then
-		umount ${DRIVE}p1
-		mkfs.vfat -F 32 -n "boot" ${DRIVE}p1
+		DRIVE1=${DRIVE}p1
 	else
 		echo "Cant find boot partition in ${DRIVE}(1|p1)"
 		exit -1
 	fi
 fi
+echo "Found first partition at ${DRIVE1}"
+umount ${DRIVE1}
+dd if=/dev/zero of=${DRIVE1} bs=512 count=1
+mkfs.vfat -F 32 -n "boot" ${DRIVE1}
 if [ -b ${DRIVE}2 ]; then
-	umount ${DRIVE}2
-	mke2fs -j -L "rootfs" ${DRIVE}2
+	DRIVE2=${DRIVE}2
 else
 	if [ -b ${DRIVE}p2 ]; then
-		umount ${DRIVE}p2
-		mke2fs -j -L "rootfs" ${DRIVE}p2
+		DRIVE2=${DRIVE}2
 	else
 		echo "Cant find rootfs partition in ${DRIVE}(2|p2)"
 		exit -1
 	fi
 fi
+echo "Found second partition at ${DRIVE2}"
+umount ${DRIVE2}
+mke2fs -j -L "rootfs" ${DRIVE2}
 }
 
 function do_mount {
-	sync
-	mkdir -p ${DST}p1
-	mkdir -p ${DST}p2
-	mount ${DRIVE}p1 ${DST}p1
-	mount ${DRIVE}p2 ${DST}p2
+umount ${DRIVE1}
+umount ${DRIVE2}
+mkdir -p ${DST}-1
+mkdir -p ${DST}-2
+mount ${DRIVE1} ${DST}-1
+mount ${DRIVE2} ${DST}-2
 }
 
 function do_copy {
@@ -75,7 +86,7 @@ if [ "x$MLO" = "x" ]; then
 	MLO=${SRC}p1/MLO
 fi;
 if [ -e $MLO ]; then
-	cp -v $MLO ${DST}p1/MLO
+	cp -v $MLO ${DST}-1/MLO
 else
 	echo "Cannot find MLO at $MLO"
 	exit -1
@@ -84,7 +95,7 @@ if [ "x$UBOOT" = "x" ]; then
 	UBOOT=${SRC}p1/u-boot.bin
 fi;
 if [ -e $UBOOT ]; then
-	cp -v $UBOOT ${DST}p1/u-boot.bin
+	cp -v $UBOOT ${DST}-1/u-boot.bin
 else
 	echo "Cannot find u-boot.bin at $UBOOT"
 	exit -1
@@ -93,7 +104,7 @@ if [ "x$UIMAGE" = "x" ]; then
 	UIMAGE=${SRC}p1/uImage
 fi;
 if [ -e $UIMAGE ]; then
-	cp -v $UIMAGE ${DST}p1/uImage
+	cp -v $UIMAGE ${DST}-1/uImage
 else
 	echo "Cannot find uImage at $UIMAGE"
 	exit -1
@@ -102,18 +113,18 @@ if [ "x$BOOTSCR" = "x" ]; then
 	BOOTSCR=${SRC}p1/boot.scr
 fi;
 if [ -e $BOOTSCR ]; then
-	cp -v $BOOTSCR ${DST}p1/boot.scr
+	cp -v $BOOTSCR ${DST}-1/boot.scr
 fi
 if [ "x$RAMDISK" != "x" && -e $RAMDISK ]; then
-	cp -v $RAMDISK ${DST}p1/ramdisk.gz
+	cp -v $RAMDISK ${DST}-1/ramdisk.gz
 fi
 if [ "x$USERSCR" != "x" && -e $USERSCR ]; then
-	cp -v $USERSCR ${DST}p1/user.scr
+	cp -v $USERSCR ${DST}-1/user.scr
 fi
 }
 
-do_clean
+#do_clean
 do_format
-#do_mount
+do_mount
 #do_wget
 #do_copy
