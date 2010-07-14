9117b04dc781b6d4fdf6d8195fd7672df92b617a
Jason Kridner <jkridner@beagleboard.org>
2010-07-14 05:03:20 -0500

    ec2build: Added s3 fuse storage

--- ec2build.sh
+++ ec2build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # This file is based on:
-#  http://developer.amazonwebservices.com/connect/message.jspa?messageID=42535#42535
-#  http://blog.atlantistech.com/index.php/2006/10/04/amazon-elastic-compute-cloud-walkthrough/
-#  http://developer.amazonwebservices.com/connect/entry.jspa?categoryID=116&externalID=661
-#  http://overstimulate.com/articles/2006/08/24/amazon-does-it-again.html
-#  http://www.howtoforge.com/amazon_elastic_compute_cloud_qemu
-#  http://info.rightscale.com/2007/2/14/bundling-up-an-ubuntu-ec2-instance
-#  http://repository.maemo.org/stable/3.1/INSTALL.txt
 #  http://code.google.com/p/maemo-sdk-image/
+#  http://forum.nginx.org/read.php?26,12659,13302
 
 # Need to get settings for
 #  EC2_HOME
@@ -23,6 +17,7 @@ source $HOME/secret/setup_env.sh
 KEYPAIR=ec2build-keypair
 KEYPAIR_FILE=$HOME/secret/$KEYPAIR.txt
 INSTANCES=$HOME/ec2build-instances.txt
+VOLUMES=$HOME/ec2build-volumes.txt
 
 AMI_UBUNTU_10_04_64BIT=ami-fd4aa494
 AMI_BEAGLEBOARD=ami-e00de889
@@ -30,6 +25,7 @@ DEFAULT_AMI=$AMI_UBUNTU_10_04_64BIT
 #DEFAULT_AMI=$AMI_BEAGLEBOARD
 MACH_TYPE=m1.large
 USER=ubuntu
+DOWNLOAD_EBS=vol-f0402d99
 
 THIS_FILE=$0
 
@@ -123,15 +119,13 @@ find-instance $AMI
 ec2-terminate-instances $INSTANCE;
 }
 
+# target local
 function disable-dash {
 sudo aptitude install expect -y
-expect <<EOF
-spawn dpkg-reconfigure -freadline dash
-send "n\n"
-interact
-EOF
+expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
 }
 
+# target local
 function enable-oe {
 cd $HOME
 disable-dash
@@ -146,17 +140,66 @@ cd angstrom-setup-scripts
 ./oebb.sh update
 }
 
+# target local
 function oebb {
 cd $HOME/angstrom-setup-scripts
 ./oebb.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
 }
 
+# host-only
 function remote-oebb {
 remote oebb $1 $2 $3 $4 $5 $6 $7 $8
 }
 
+# host-only
 function build-default {
 remote-oebb bitbake console-image
+}
+
+function create-download-ebs {
+# VOLUME  vol-10402d79    10              us-east-1c      creating        2010-07-14T08:21:14+0000
+DOWNLOAD_EBS=`ec2-create-volume -s 10 -z us-east-1c | perl -ne '/^VOLUME\s+(\S+)\s+/ && print "$1"'`
+echo DOWNLOAD_EBS=$DOWNLOAD_EBS
+}
+
+function attach-ebs-ami {
+EBS_VOLUME=$1
+DEVICE=$2
+AMI=$3
+find-instance $AMI
+VOLUME_STATUS=
+while
+ [ ! "x$VOLUME_STATUS" = "xavailable" ]
+do
+ ec2-describe-volumes | tee $VOLUMES;
+ #VOLUME  vol-b629ccdf    200             us-east-1c      in-use
+ VOLUME_STATUS=`perl -ne '/^VOLUME\s+'${EBS_VOLUME}'\s+\S+\s+\S+\s+(\S+)/ && print "$1"' $VOLUMES`
+ echo VOLUME_STATUS=$VOLUME_STATUS
+done
+ec2-attach-volume $EBS_VOLUME -i $INSTANCE -d $DEVICE
+}
+
+# target local
+function mount-ebs-ami {
+EBS_VOLUME=$1
+DEVICE=$2
+DIRNAME=$3
+AMI=$4
+find-instance $AMI
+VOLUME_STATUS=
+while
+ [ ! "x$VOLUME_STATUS" = "xattached" ]
+do
+ ec2-describe-volumes | tee $VOLUMES;
+ #ATTACHMENT      vol-f0402d99    i-2fd61c45      /dev/sdd        attaching       2010-07-14T08:53:30+
+ VOLUME_STATUS=`perl -ne '/^ATTACHMENT\s+'${EBS_VOLUME}'\s+'${INSTANCE}'\s+\S+\s+(\S+)/ && print "$1"' $VOLUMES`
+ echo VOLUME_STATUS=$VOLUME_STATUS
+done
+}
+
+function mount-download-ebs {
+#attach-ebs-ami $DOWNLOAD_EBS /dev/sdd
+mount-ebs-ami $DOWNLOAD_EBS /dev/sdd 
 }
 
 function bundle-vol {
@@ -168,6 +211,23 @@ ec2-upload-bundle -b $S3_BUCKET -m /mnt/$IMAGE_NAME.manifest.xml -a $AWS_ID -s $
 
 function publish {
 ec2-register $S3_BUCKET/$1.manifest.xml
+}
+
+# http://xentek.net/articles/448/installing-fuse-s3fs-and-sshfs-on-ubuntu/
+function enable-s3fuse {
+sudo aptitude install build-essential libcurl4-openssl-dev libxml2-dev libfuse-dev comerr-dev libfuse2 libidn11-dev libkadm55 libkrb5-dev libldap2-dev libselinux1-dev libsepol1-dev pkg-config fuse-utils sshfs -y
+wget http://s3fs.googlecode.com/files/s3fs-r177-source.tar.gz
+tar xzvf s3fs-r177-source.tar.gz
+cd ./s3fs
+sudo make
+sudo make install
+sudo perl -pe 's/^#user_allow_other/user_allow_other/' -i.bak /etc/fuse.conf
+}
+
+function mount-s3 {
+sudo mkdir -p /mnt/s3
+sudo modprobe fuse
+sudo s3fs angstrom-builds -o accessKeyId=$AWS_ID -o secretAccessKey=$AWS_PASSWORD -o use_cache=/tmp -o allow_other /mnt/s3
 }
 
 $*
