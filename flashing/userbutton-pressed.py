4b1fbdfdd37dbd44867b67ac5da642c4061c0ff2
Joel A Fernandes <agnel.joel@gmail.com>
2011-07-07 22:25:53 -0500

    mechomaniac's User-button script that checks for an input event
    Signed-off-by: Joel A Fernandes <agnel.joel@gmail.com>

---
+++ flashing/userbutton-pressed.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Beagleboard user button
+# Copyright 2009 mechomaniac.com
+import struct
+inputDevice = "/dev/input/event0"
+# format of the event structure (int, int, short, short, int)
+inputEventFormat = 'iihhi'
+inputEventSize = 16
+ 
+file = open(inputDevice, "rb") # standard binary file input
+event = file.read(inputEventSize)
+while event:
+  (time1, time2, type, code, value) = struct.unpack(inputEventFormat, event)
+  if type == 1 and code == 276 and value == 1:
+     print "User button pressed!"
+  event = file.read(inputEventSize)
+file.close()
