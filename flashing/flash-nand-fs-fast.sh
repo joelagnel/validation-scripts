3653c5350eac4146fc41735100a1c0004d1c9271
Joel A Fernandes <agnel.joel@gmail.com>
2011-07-07 22:23:50 -0500

    flashing scripts: User-button determines whether to flash
        * Added user.txt and uEnv.txt
          - user.txt does the flashing and is used only when the user-button is pressed
          - uEnv.txt does a regular boot is used otherwise
    
        * Flash script
          - perform flashing on boot only if user button is pressed
    
    Signed-off-by: Joel A Fernandes <agnel.joel@gmail.com>

--- flashing/flash-nand-fs-fast.sh
+++ flashing/flash-nand-fs-fast.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Determine user-button value
+echo 7 > /sys/class/gpio/unexport
+echo 7 > /sys/class/gpio/export
+
+if [ $? != "0" ]; then
+  echo "Error exporting GPIO value"
+  exit
+fi
+
+if [ ! -e /sys/class/gpio/gpio7/value ]; then
+  echo "Couldn't determine user-button value, value file doesn't exit"
+  exit
+fi
+
+USER_BUTTON_PRESSED=`cat /sys/class/gpio/gpio7/value`
+
+if [ "x$USER_BUTTON_PRESSED" = "x0" ]; then
+  exit
+fi
+
+
 # This script is to be run from an init script once the board has booted
 if [ ! -e /dev/mtd4 ]; then
   echo "ERROR: No NAND Filesystem partition found. Aborting flashing."
