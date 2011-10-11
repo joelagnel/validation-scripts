c08fa78a5a18c5dcaf1f93fa553d2d9b979fadd6
Joel A Fernandes <agnel.joel@gmail.com>
2011-10-11 16:37:41 -0500

    ethernet: First version of auto ethernet tests
    Configuration
    Bone -> USB -> Hub -> Ethernet -> Bone
    
    Broadcast packets on the network and make sure we receive them on the other end
    
    Signed-off-by: Joel A Fernandes <agnel.joel@gmail.com>

---
+++ bone-tester/component/ethernet.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Important to Error Out on any errors
+set -e
+
+HOST_DEV=eth0
+HOST_IP="192.168.100.1"
+
+HUB_DEV=eth1
+HUB_IP="192.168.100.2"
+
+HOST_RX_PACKETS=/sys/class/net/eth0/statistics/rx_packets
+HUB_RX_PACKETS=/sys/class/net/eth1/statistics/rx_packets
+
+PING_INTERVAL=1
+PING_COUNT=10
+
+# Load kernel modules
+# Load required kernel modules for test purposes
+# This will go away once PSP fixes the OTG host-mode issue.
+
+# Load modules in the right order
+echo " Removing kernel modules.."
+rmmod g_zero			|| true
+rmmod g_file_storage	|| true
+rmmod g_ether			|| true
+
+# USB0 Gadget
+echo "Loading USB kernel modules.."
+modprobe g_ether
+# Turn on USB1 Host for ethernet
+modprobe g_zero
+
+sleep 5		# Wait for the kernel to do its job of detecting the interfaces, registering drivers etc.
+
+ifconfig ${HOST_DEV} ${HOST_IP} up
+ifconfig ${HUB_DEV} ${HUB_IP} up
+
+old_rx=$(cat ${HOST_RX_PACKETS})
+
+ping -b -I ${HUB_DEV} -i ${PING_INTERVAL} 255.255.255.0 -c ${PING_COUNT} || true
+
+new_rx=$(cat ${HOST_RX_PACKETS})
+
+total_rx=$(echo "${new_rx}-${old_rx}" | bc)
+
+deviation=$(echo "${PING_COUNT}-${total_rx}" | bc)
+
+if [ ${deviation} -gt 2 ] ; then
+	echo "Packet loss encountered, ethernet test fail."
+	exit 1
+fi
+
+echo "Ethernet test passed!"
+
