764cc22b3735c1106a0cef7de72dbe02ba5bdf55
Joel A Fernandes <agnel.joel@gmail.com>
2011-10-10 20:34:31 -0500

    usb: Added status messages
    Signed-off-by: Joel A Fernandes <agnel.joel@gmail.com>

--- bone-tester/component/usb_loopback.sh
+++ bone-tester/component/usb_loopback.sh
@@ -9,7 +9,7 @@ COMPONENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 G_FILE_STORAGE_NAME=${COMPONENT_DIR}/data/backing-file
 G_FILE_BACKING_SIZE_M=20
 G_FILE_BACKING_DEV=/dev/sda
-G_FILE_BACKING_MNT=/tmp/mnt
+G_FILE_BACKING_MNT=/home/root/mnt-gadget
 
 # Check if backing file exists
 if ! [ -e ${G_FILE_STORAGE_NAME} ] ; then
@@ -21,16 +21,18 @@ fi
 # This will go away once PSP fixes the OTG host-mode issue.
 
 # Load modules in the right order
+echo " Removing kernel modules.."
 rmmod g_zero			|| true
 rmmod g_file_storage	|| true
 rmmod g_ether			|| true
 
 # USB0 Gadget
+echo "Loading USB kernel modules.."
 modprobe g_file_storage file=${G_FILE_STORAGE_NAME}
-
 # Turn on USB1 Host
 modprobe g_zero
 
+echo "Checking if loaded correctly.."
 if [ "x$(lsmod | grep g_zero)" = "x" ] ; then
 	echo "Failed to load kernel modules."
 	exit $?
@@ -40,33 +42,44 @@ if [ "x$(lsmod | grep g_file_storage)" = "x" ] ; then
 	exit $?
 fi
 
+echo "Syncing to get gadget block device to showup.."
 sync	# This makes sure the filesystem state is reflected correctly
 		# Without sync'ing, the gadget device doesn't show up
 
+echo "Checking if gadget device is up"
 # FIXME: dev name should be auto detected
 if ! [ -e /dev/sda ] ; then
 	echo "Gadget FS Device didn't come up.. waiting for a few seconds"
 	sleep 5
-fi
-if ! [ -e /dev/sda ] ; then
-	echo "Gadget FS Device didn't come up"
-	exit 1
+	if ! [ -e /dev/sda ] ; then
+		echo "Gadget FS Device didn't come up"
+		exit 1;
+	else
+		echo "Now it did."
+	fi
 fi
 
+echo "Gadget Mounting and test file creation.."
+rm -rf ${G_FILE_BACKING_MNT}
 mkdir -p ${G_FILE_BACKING_MNT}
 mount /dev/sda ${G_FILE_BACKING_MNT}
 
 dd if=/dev/urandom of=/tmp/test-file bs=4k count=$((256 * 4))
+
+echo "Copying test file.."
 cp /tmp/test-file ${G_FILE_BACKING_MNT}/
 
 umount ${G_FILE_BACKING_MNT}
 mount /dev/sda ${G_FILE_BACKING_MNT}
 
+echo "Check if file is copied properly.."
 if [ "x$(diff ${G_FILE_BACKING_MNT}/test-file /tmp/test-file)" != "x" ] ; then
 	echo "Data copied onto Gadget FS not present or corrupt"
+	exit 1
 fi
 
 umount ${G_FILE_BACKING_MNT}
+rm -rf ${G_FILE_BACKING_MNT}
 rm /tmp/test-file
 
 echo "USB Host/Device Test Passed!"
